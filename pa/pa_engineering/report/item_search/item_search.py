dde041ceda4e9c9935435483641d8e6d52ec33e2
Hayden Cheers <hayden.cheers@me.com>
2019-06-27 17:39:50 +1000

    changes

--- pa/pa_engineering/report/item_search/item_search.py
+++ pa/pa_engineering/report/item_search/item_search.py
@@ -6,12 +6,12 @@ import frappe
 from frappe import _
 
 def execute(filters=None):
-	filters = frappe._dict(filters or {})
-	columns = get_columns(filters)
+	filters = get_filters(frappe._dict(filters or {}))
+	columns = get_columns()
 	data = get_data(filters)
 	return columns, data
 
-def get_columns(filters):
+def get_columns():
 	columns = [
 		{
 			"label": _("Item Code"),
@@ -34,20 +34,6 @@ def get_columns(filters):
 			"width": 200
 		},
 		{
-			"label": _("Brand"),
-			"fieldtype": "Link",
-			"fieldname": "brand",
-			"options": "Brand",
-			"width": 200
-		},
-		{
-			"label": _("Supplier"),
-			"fieldtype": "Link",
-			"fieldname": "supplier",
-			"options": "Supplier",
-			"width": 200
-		},
-		{
 			"label": _("Buying Price"),
 			"fieldtype": "Currency",
 			"fieldname": "buying_price",
@@ -62,46 +48,60 @@ def get_columns(filters):
 			"width": 200
 		},
 		{
-			"label": _("Selling 1.6"),
+			"label": _("Selling@1.6"),
 			"fieldtype": "Currency",
 			"fieldname": "selling_price_16",
 			"options": "AUD",
 			"width": 200
 		},
 		{
-			"label": _("Selling 1.8"),
+			"label": _("Selling@1.8"),
 			"fieldtype": "Currency",
 			"fieldname": "selling_price_18",
 			"options": "AUD",
 			"width": 200
 		},
 		{
-			"label": _("Selling 2.0"),
+			"label": _("Selling@2.0"),
 			"fieldtype": "Currency",
 			"fieldname": "selling_price_20",
 			"options": "AUD",
 			"width": 200
-		}
+		},
+				{
+			"label": _("Brand"),
+			"fieldtype": "Link",
+			"fieldname": "brand",
+			"options": "Brand",
+			"width": 200
+		},
+		{
+			"label": _("Supplier"),
+			"fieldtype": "Link",
+			"fieldname": "supplier",
+			"options": "Supplier",
+			"width": 200
+		},
 	]
 	return columns
 
 def surround_wildcard(str):
 	return "%" + str + "%"
 
-def get_conditions(filters):
+def get_filters(filters):
 	conditions = []
 
-	if filters.item_code:
+	"""if filters.item_code:
 		item_code = filters.item_code
 		segments = item_code.split(" ")
 		wildly = map(surround_wildcard, segments)
 		wildcards = "".join(segmeents)
-		conditions.append("i.item_code COLLATE UTF8_GENERAL_CI LIKE ")
+		conditions.append("i.item_code COLLATE UTF8_GENERAL_CI LIKE ")"""
 
 	return conditions
 
 def get_data(conditions):
-	data = []
+	data = [{"item_code": "a"}]
 
 	sql_query = 	"""
 			SELECT * FROM `tabItem` i
